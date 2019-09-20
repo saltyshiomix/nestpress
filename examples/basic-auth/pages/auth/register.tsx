2b8ebbb0422137387ff8552e0657e44347485b97
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-20 20:06:42 +0900

    Remove unused variables

--- examples/basic-auth/pages/auth/register.tsx
+++ examples/basic-auth/pages/auth/register.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
