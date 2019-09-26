79315e1382d59fa0c93f6fdc853bed1d0745325d
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-26 19:42:45 +0900

    Refactoring: rename

--- server/routes/route.module.ts
+++ server/routes/route.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from '../logics/auth/auth.module';
 import { ArticleModule } from '../logics/article/article.module';
 import { ApiAuthController } from './api/auth.controller';
 import { ApiAdminMeController } from './api/admin/me.controller';
-import { ApiArticleController } from './api/public/article.controller';
+import { ApiArticleController } from './api/general/article.controller';
 import { AdminController } from './admin.controller';
 import { HomeController } from './home.controller';
 
