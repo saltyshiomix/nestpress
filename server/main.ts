964968719641bd6a2d09c062324681faa3c06430
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-26 20:43:40 +0900

    Add helmet for the better security

--- server/main.ts
+++ server/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.use(require('helmet')());
+
   app.get(LogicModule).initialize(app);
 
   app.get(NextModule).prepare().then(() => {
