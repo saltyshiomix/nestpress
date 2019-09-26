58d04fdae77177394d2d0d62a1f5e0a41bd355bc
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-27 02:24:10 +0900

    Add dummy data

--- server/routes/api/general/article.controller.ts
+++ server/routes/api/general/article.controller.ts
@@ -27,7 +27,28 @@ export class ApiArticleController {
         title: 'Sophisticated Dark Theme',
         description: 'Soft Paper Style Articles that is Easy on the Eyes',
       },
+      {
+        id: 3,
+        title: 'The Production Ready Personal Blogging System',
+        description: 'On Top of NestJS, NEXT.js and Material UI',
+      },
+      {
+        id: 4,
+        title: 'Sophisticated Dark Theme',
+        description: 'Soft Paper Style Articles that is Easy on the Eyes',
+      },
+      {
+        id: 5,
+        title: 'The Production Ready Personal Blogging System',
+        description: 'On Top of NestJS, NEXT.js and Material UI',
+      },
+      {
+        id: 6,
+        title: 'Sophisticated Dark Theme',
+        description: 'Soft Paper Style Articles that is Easy on the Eyes',
+      },
     ];
+
     res.json(articles);
   }
 }
