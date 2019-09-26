6d635b1c3f3082d8e1c27c5620409eedefb24982
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-26 20:26:42 +0900

    Move the logout button to the AppBar

--- components/Page/Admin/Home/index.tsx
+++ components/Page/Admin/Home/index.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import {
-  Button,
+  Typography,
   Card,
   CardContent,
-  Typography,
 } from '@material-ui/core';
 import { AdminLayout } from '../../../Layout';
-import { Http } from '../../../../lib';
-
-const http = new Http();
 
 export const AdminHomePageComponent = (props) => {
-  const onClickLogout = async (e: React.MouseEvent) => {
-    e.preventDefault();
-    const isLoggedOut: boolean = await http.post('api/auth/logout');
-    if (isLoggedOut) {
-      location.href = '/admin/login';
-    }
-  };
-
   return (
     <AdminLayout>
       <Card>
@@ -26,16 +14,6 @@ export const AdminHomePageComponent = (props) => {
           <Typography variant="body1">
             You are now logged in as {props.user.email} :)
           </Typography>
-          <br />
-          <Button
-            type="submit"
-            variant="outlined"
-            color="primary"
-            size="large"
-            onClick={onClickLogout}
-          >
-            LOGOUT
-          </Button>
         </CardContent>
       </Card>
     </AdminLayout>
