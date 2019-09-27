7b40056b0b95d2899eaab553aeace01bd8f4fed8
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-28 04:56:10 +0900

    Create the brand const

--- lib/theme/index.ts
+++ lib/theme/index.ts
@@ -1,22 +1,18 @@
-import {
-  Theme,
-  createMuiTheme,
-} from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-export const theme: Theme = createMuiTheme({
+export const theme = createMuiTheme({
   palette: {
     type: 'dark',
-    primary: {
-      main: '#00ff00',
-    },
-    secondary: {
-      main: '#ffff00',
-    },
-    text: {
-      primary: '#f5f5f5',
-      secondary: '#00ff00',
-      disabled: '#d7d7d7',
-      hint: '#ffff00',
-    },
   },
 });
+
+const { augmentColor } = theme.palette;
+
+export const createAugmentColor = (color: string) => augmentColor({ 500: color });
+
+export const brand = {
+  primary: createAugmentColor('#00ff00'), // lime
+  secondary: createAugmentColor('#00ff00'), // yellow
+  text: createAugmentColor('#d8d8d8'), // white
+  background: createAugmentColor('#222222'), // black
+};
