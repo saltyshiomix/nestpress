8bccadc39e9b15a3737e7aa8e4a7d7c745b9fb90
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-02 02:38:57 +0900

    Refactoring: remove comments

--- lib/theme/index.ts
+++ lib/theme/index.ts
@@ -11,20 +11,16 @@ const { augmentColor } = theme.palette;
 export const createAugmentColor = (color: string) => augmentColor({ 500: color });
 
 export const brand = {
-  // lime
   primary: {
     main: '#00ff00',
   },
-  // yellow
   secondary: {
     main: '#00ff00',
   },
-  // white
   text: {
     main: '#d8d8d8',
     light: '#ffffff',
   },
-  // black
   background: {
     main: '#222222',
     light: '#333333',
