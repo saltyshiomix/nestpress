04e1a131ca4c8c9e3293bcd4581f9a7e1d225e59
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-01 14:30:55 +0900

    Add types

--- packages/next/lib/next.service.ts
+++ packages/next/lib/next.service.ts
@@ -7,7 +7,7 @@ import Server from 'next/dist/next-server/server/next-server';
 export class NextService {
   private app!: Server;
 
-  public setApp(app: Server) {
+  public setApp(app: Server): void {
     this.app = app;
   }
 
@@ -15,11 +15,11 @@ export class NextService {
     return this.app;
   }
 
-  public render(page: string, req: IncomingMessage, res: ServerResponse) {
+  public render(page: string, req: IncomingMessage, res: ServerResponse): void {
     this.app.render(req, res, page);
   }
 
-  public renderWithData(page: string, data: any, req: IncomingMessage, res: ServerResponse) {
+  public renderWithData(page: string, data: any, req: IncomingMessage, res: ServerResponse): void {
     this.app.render(req, res, page, data);
   }
 }
